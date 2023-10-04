eac8ebd9acae318a22cf02535af994336f544c93
willhamlet <82181630+willhamlet@users.noreply.github.com>
2023-10-03 22:15:33 -0300

    revisão
    dia 3

---
+++ aula22/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores lógicos
+&& -> and - Todas as expressões precisam ser verdadeiras para retornar true
+|| -> or - Só uma precisa estar verdadeira para retornar true
+! -> not - 
+
+*/
+
+
+const expressao_and = true && true && true && true;
+const expressao_or = false || true || false || false; 
+const expressao_not = !false;
+console.log(expressao_and);
+console.log(expressao_or);
+console.log(expressao_not);
+
+
---
+++ aula23/index.js
@@ -0,0 +1,14 @@
+/*
+&& ->
+||
+*/
+
+const a = 0; 
+const b = null;
+const c = 'false';
+const d = false; 
+const e = NaN;
+
+console.log(a || b || c || d || e);
+
+
